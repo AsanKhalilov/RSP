d2491a6c6104d62edd104c99d15b26d07de9c7c3
AsanKhalilov <khalilov2501@gmail.com>
2023-10-31 15:53:53 +0300

    lab2

--- lab3/decrementWorker.js
+++
@@ -1,3 +0,0 @@
-const { parentPort } = require('worker_threads');
-
-parentPort.postMessage('decrement');
--- lab3/incrementWorker.js
+++
@@ -1,3 +0,0 @@
-const { parentPort } = require('worker_threads');// определяем воркеры, которые отправляют сообщения об инкрементации 
-parentPort.postMessage('increment');
-// Эти сообщения используются для вызова функции modifyCounter, которая изменяет общий счетчик
